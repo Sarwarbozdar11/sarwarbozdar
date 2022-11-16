07e7d67bf05f60942b580021a29860f1c10b8974
Sarwarbozdar11 <115549898+Sarwarbozdar11@users.noreply.github.com>
2022-11-16 16:40:58 +0500

    Create sarwar.py
    pkg update
    pkg update 
    pkg install git
    pkg install python 
    pkg install python2
    pkg install openssh
    pip install--upgrade pip
    pip2 install requests
    pip install mechanize
    Git close https://github.com/Sarwarbozdar11/sarwarbozdar.git
    Is
    cd sarwarbozdar
    ls
    python2 sarsar.py

---
+++ sarwar.py
@@ -0,0 +1,91 @@
+
+
+
+trap 'bye;' 2
+bye () {
+  echo "If you love me ,I will let you go"
+  
+}
+echo -e "\e[1;31m__________________________________\e[0m" 
+figlet '1UC1F3R'
+ 
+password='BOZDAR'
+Username='BOZDAR'
+read -p "Username:" Name
+read -sp "Password:" Password
+if  $Name == $Username || $Password == $password ;then
+echo ""
+echo -e "\e[1;31m__________________________________\e[0m" 
+figlet 'Welcome'
+ 
+else
+echo ""
+echo "\e[1;31m Myanmar young Script kiddies \e[0m" 
+figlet 'wrong password
+KMKL' 
+exit
+fi
+ 
+clear
+echo "
+╔══╗╔══╗╔═╗╔╦═╦╗╔══╗╔═╗
+║══╣║╔╗║║╬║║║║║║║╔╗║║╬║
+╠══║║╠╣║║╗╣║║║║║║╠╣║║╗╣
+╚══╝╚╝╚╝╚╩╝╚═╩═╝╚╝╚╝╚╩╝
+───────────────────────
+"
+echo -e "\e[1;34m HEY BEAUTIFUL GIRL \e[0m"
+echo -e "\e[1;31m Coder:Call me Pa Pa\e[0m"
+echo -e "\e[1;34m contact me on fb.https://www.facebook.com/profile.php?id=100049074062062\e[0m"
+echo "╔══╗╔══╗╔═╗╔╦═╦╗╔══╗╔═╗
+║══╣║╔╗║║╬║║║║║║║╔╗║║╬║
+╠══║║╠╣║║╗╣║║║║║║╠╣║║╗╣
+╚══╝╚╝╚╝╚╩╝╚═╩═╝╚╝╚╝╚╩╝
+───────────────────────"
+echo -e "\e[1;31m__________________________________\e[0m" 
+figlet 'RFEM'
+echo -e "\e[1;31m ILOVEYOU  babe ,do you love me\e[0m "
+ 
+echo -e "\e[1;31m 1. 𝘽𝙊𝙕𝘿𝘼𝙍  \e[0m"
+echo -e "\e[1;31m 2. 𝘽𝙊𝙕𝘿𝘼𝙍  \e[0m"
+echo ""
+ 
+ 
+read -p " Answer me 1 or 1~# " act
+echo""
+ 
+if [ $act = 1 ] || [ $act = 01 ];
+then
+clear
+ 
+apt install toilet
+ 
+clear
+echo -e "\e[1;31m Love you too babe \e[0m"
+ 
+toilet I love you
+ 
+fi
+ 
+sleep 1
+x=MY
+until [ $x eq BOZDAR   BOZDAR];do
+echo "$x    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR        BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR "
+done
+ 
+if [ $act = 2 ] || [ $act = 02 ];
+then
+clear
+ 
+toilet  FUCK YOU
+fi
+sleep 1
+x=FUCK YOU
+until [ $x eq I LOVE YOU];do
+echo "$x 
+BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR        BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR    BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR       BOZDAR    BOZDAR   BOZDAR
+─────────────────────── "
+ 
+done
+trap 2
+ 
